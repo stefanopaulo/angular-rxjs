e622b728877ec4f7d0a178298afac0915bab7b0e
Stefano Paulo <stefanopaulo47@gmail.com>
2021-07-02 17:12:28 -0300

    operador map

--- src/app/acoes/acoes.service.ts
+++ src/app/acoes/acoes.service.ts
@@ -1,6 +1,8 @@
 import { environment } from './../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators'
+import { Acao } from './modelo/acoes';
 
 const API = environment.api;
 
@@ -12,6 +14,20 @@ export class AcoesService {
   constructor(private http: HttpClient) { }
 
   getAcoes() {
-    return this.http.get<any>(`${API}/acoes`);
+    return this.http.get<any>(`${API}/acoes`).pipe(
+      map((acoes) => acoes.sort((acaoA, acaoB) => this.ordenaPorCodigo(acaoA, acaoB)))
+    );
+  }
+
+  private ordenaPorCodigo(acaoA: Acao, acaoB: Acao) {
+    if (acaoA.codigo > acaoB.codigo) {
+      return 1;
+    }
+
+    if (acaoB.codigo > acaoA.codigo) {
+      return -1;
+    }
+
+    return 0;
   }
 }
