73d43870055017769ce338c1ad1d8b4d6b079a8a
Stefano Paulo <stefanopaulo47@gmail.com>
2021-07-02 17:56:56 -0300

    filter

--- src/app/acoes/acoes.component.ts
+++ src/app/acoes/acoes.component.ts
@@ -1,7 +1,7 @@
 import { AcoesService } from './acoes.service';
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { switchMap, tap } from 'rxjs/operators';
+import { filter, switchMap, tap } from 'rxjs/operators';
 import { merge } from 'rxjs';
 
 @Component({
@@ -13,6 +13,7 @@ export class AcoesComponent {
   acoesInput = new FormControl();
   todasAcoes$ = this.acoesService.getAcoes();
   filtroInput$ = this.acoesInput.valueChanges.pipe(
+    filter((valorDigitado) => valorDigitado.length >= 3 || !valorDigitado.length),
     switchMap((valorDigitado) => this.acoesService.getAcoes(valorDigitado))
   );
 
